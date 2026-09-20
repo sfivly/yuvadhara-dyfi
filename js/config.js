57cc73d16ff9cdae833cbed6e6daf61725f24c18
sfivly <sfivly@gmail.com>
2026-09-20 16:18:05 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Google Apps Script Web App URL (deployment: AKfycbxevhUjwmI918bt-NvhAI8QIi5jmJQAzbwTE1_gKuhtB60g6KnJltlNuda3B_89SnQsxg)
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxevhUjwmI918bt-NvhAI8QIi5jmJQAzbwTE1_gKuhtB60g6KnJltlNuda3B_89SnQsxg/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzOt7PJe8tW5gsdrB9SFzh-Nu2ks411dEbdT5sVuKmdNeDwubfYwtWoMeZU42XDqlOE/exec";

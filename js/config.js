a1c7ef828e6670d5e701350e033087fb43582b2f
sfivly <sfivly@gmail.com>
2026-09-20 15:31:24 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,2 @@
+// Google Apps Script Web App URL (deployment: AKfycbxevhUjwmI918bt-NvhAI8QIi5jmJQAzbwTE1_gKuhtB60g6KnJltlNuda3B_89SnQsxg)
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxevhUjwmI918bt-NvhAI8QIi5jmJQAzbwTE1_gKuhtB60g6KnJltlNuda3B_89SnQsxg/exec";
